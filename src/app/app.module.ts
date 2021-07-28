78b7afd57ee5e2452f8a7e0571f335a49a3d88a8
Pavan Babul Gowd Relangi <11202132@in.techwave.net>
2021-07-28 15:56:26 +0530

    user-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{AngmaterialModule} from './ANGMATERIAL/angmaterial.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr'; 
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { ToastrModule } from 'ngx-toastr';
   ],
   imports: [
     BrowserModule,ToastrModule.forRoot(),
-    AppRoutingModule,HttpClientModule
+    AppRoutingModule,HttpClientModule,AngmaterialModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/COMMON/header/header.component.ts
+++ src/app/user/COMMON/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+    import {​​​ MatDialog }​​​ from'@angular/material/dialog';
+
 import { CartService } from 'src/app/SERVICES/cart.service';
+import { UserprofileComponent } from '../userprofile/userprofile.component';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +13,24 @@ import { CartService } from 'src/app/SERVICES/cart.service';
 export class HeaderComponent implements OnInit {
 login:string="Sign in";
 // cartCount:number=0;
-// public totalItem:number=0;
-//   constructor() { }
 
-//   ngOnInit(): void {
-//   }
 public totalItem : number = 0;
-constructor(private cartService : CartService) { }
+constructor(private cartService : CartService,public dialog: MatDialog) { }
 
-ngOnInit(): void {
+ngOnInit() {
   this.cartService.getProducts()
   .subscribe(res=>{
     this.totalItem = res.length;
   })
 }
+
+
+openDialog() {​​​
+const dialogRef = this.dialog.open(UserprofileComponent);
+dialogRef.afterClosed().subscribe(result=> {​​​
+console.log(`Dialog result: ${​​​result}​​​`);
+    }​​​);
+  }​​​
+
+
 }
